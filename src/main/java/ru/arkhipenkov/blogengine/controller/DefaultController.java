c52eb05c9b332cfa00f164a0ac4a7a19aecda7a5
archipenkov@tut.by <4uEYjUg9s!u85LN>
2021-12-06 15:28:43 +0300

    добавление проперти application.yml замена RestController на Controller закоментирование security в pom

--- src/main/java/ru/arkhipenkov/blogengine/controller/DefaultController.java
+++ src/main/java/ru/arkhipenkov/blogengine/controller/DefaultController.java
@@ -1,10 +1,10 @@
 package ru.arkhipenkov.blogengine.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DefaultController {
 
   @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\.]*}")
